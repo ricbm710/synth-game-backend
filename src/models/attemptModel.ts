090b181159602cbff2c63c033a9223473bf49f02
Ricardo <ricardobarmont@gmail.com>
2025-04-03 15:45:30 -0400

    changed leaderboard query. Only one entry per player

--- src/models/attemptModel.ts
+++ src/models/attemptModel.ts
@@ -14,7 +14,7 @@ export const createAttemptInDb = async (player_id: number, score: number) => {
 export const getLeaderboardFromDb = async (): Promise<Leaderboard[]> => {
   //* limit should vary
   const result = await query<Leaderboard>(
-    "select p.player_name as Player, a.score as Score, to_char(a.time_started, 'Mon/DD/YYYY') as Date, to_char(a.time_started, 'HH24:MI') as Time from attempts a, players p where a.player_id = p.id order by a.score desc limit 20"
+    "SELECT DISTINCT ON (p.id) p.player_name AS Player, a.score AS Score, TO_CHAR(a.time_started, 'Mon DD, YYYY') AS Date, TO_CHAR(a.time_started, 'HH24:MI') AS UTC_Time FROM attempts a JOIN players p ON a.player_id = p.id ORDER BY p.id, a.score DESC LIMIT 10"
   );
   return result.rows;
 };
