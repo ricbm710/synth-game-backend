9ee748278d1c9c7a18d72ced5adf7ea3304757d5
Ricardo <ricardobarmont@gmail.com>
2025-04-03 16:41:56 -0400

    fixed Leaderboard query error

--- src/models/attemptModel.ts
+++ src/models/attemptModel.ts
@@ -14,7 +14,7 @@ export const createAttemptInDb = async (player_id: number, score: number) => {
 export const getLeaderboardFromDb = async (): Promise<Leaderboard[]> => {
   //* limit should vary
   const result = await query<Leaderboard>(
-    "SELECT DISTINCT ON (p.id) p.player_name AS Player, a.score AS Score, TO_CHAR(a.time_started, 'Mon DD, YYYY') AS Date, TO_CHAR(a.time_started, 'HH24:MI') AS UTC_Time FROM attempts a JOIN players p ON a.player_id = p.id ORDER BY p.id, a.score DESC LIMIT 10"
+    "SELECT * FROM (SELECT DISTINCT ON (p.id) p.player_name AS Player, a.score AS Score, TO_CHAR(a.time_started, 'Mon DD, YYYY') AS Date, TO_CHAR(a.time_started, 'HH24:MI') AS UTC_Time FROM attempts a JOIN players p ON a.player_id = p.id ORDER BY p.id, a.score DESC, a.time_started DESC) sub ORDER BY Score DESC LIMIT 20"
   );
   return result.rows;
 };
